c306a3ee68a1dc2f7a1af6afb1b3e1080b681bb4
tejas1 <suryateja.malisetti@pega.com>
2021-04-22 22:13:36 +0530

    test commit

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -8,6 +8,17 @@ const MyMessage = ({ message }) => {
       />
     )
   }
+
+  // needs to be removed
+  if(message?.attachments?.length > 0) {
+    return (
+      <img src={message.attachments[0].file}
+        alt='message-attachment'
+        className="message-image"
+        style={{float: 'right'}}
+      />
+    )
+  }
   return(
     <div className="message" style={{ float: 'right', marginRight: '18px', color:'white', backgroundColor:'#3B2A50'}}>
       {message.text}
